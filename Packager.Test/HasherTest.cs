7fc535605f5f97cc5ef893b04657b3620286f0e2
BrianMcGough <bmcgough@iu.edu>
2015-05-21 17:02:21 -0400

    finished up first unit test

---
+++ Packager.Test/HasherTest.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Packager.Utilities;
+
+namespace Packager.Test
+{
+    [TestFixture]
+    public class HasherTest
+    {
+        [Test]
+        public void OutputShouldBe32Chars()
+        {
+            var bytes = new byte[] {24, 16, 8, 32};
+            var hasher = new Hasher();
+            var result = hasher.Hash(new MemoryStream(bytes));
+            Assert.That(result.Length, Is.EqualTo(32));
+        }
+
+        [Test]
+        public void OutputShouldOnlyContainCorrectCharacters()
+        {
+            var bytes = new byte[] {24, 16, 8, 32};
+            var hasher = new Hasher();
+            var result = hasher.Hash(new MemoryStream(bytes));
+            var acceptableChars = new[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+            foreach (var c in result.ToCharArray())
+            {
+                Assert.That(acceptableChars.Contains(c), Is.True);
+            }
+        }
+    }
+}
---
+++ Packager/Utilities/IHasher.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Packager.Utilities
+{
+    public interface IHasher
+    {
+        string Hash(Stream content);
+    }
+
+    public class Hasher : IHasher
+    {
+        public string Hash(Stream content)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (content)
+                {
+                    var hash = md5.ComputeHash(content);
+                    return hash.Aggregate(string.Empty, (current, b) => current + string.Format("{0:x2}", b));
+                }
+            }
+        }
+    }
+}

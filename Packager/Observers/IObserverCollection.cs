76491d3a41d511471c745fe28c522c017d2604db
Brent Moberly <bmoberly@iu.edu>
2015-07-27 10:48:59 -0400

    adding observer collection
    will make things more testable if we replace extension methods with
    observer collection

---
+++ Packager/Observers/IObserverCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packager.Observers
+{
+    public interface IObserverCollection: IList<IObserver>
+    {
+        void Log(string baseMessage, params object[] elements);
+        void LogIssue(Exception issue);
+        Guid BeginSection(string baseMessage, params object[] elements);
+        void EndSection(Guid sectionKey, string newTitle = "", bool collapse = false);
+    }
+}
---
+++ Packager/Observers/ObserverCollection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Packager.Exceptions;
+
+namespace Packager.Observers
+{
+    public class ObserverCollection : List<IObserver>, IObserverCollection
+    {
+        public void Log(string baseMessage, params object[] elements)
+        {
+            foreach (var observer in this)
+            {
+                observer.Log(baseMessage, elements);
+            }
+        }
+
+        public void LogIssue(Exception issue)
+        {
+            if (issue is LoggedException)
+            {
+                return;
+            }
+
+            foreach (var observer in this)
+            {
+                observer.LogError(issue);
+            }
+        }
+
+        public Guid BeginSection(string baseMessage, params object[] elements)
+        {
+            var sectionKey = Guid.NewGuid();
+            foreach (var observer in ViewModelObservers())
+            {
+                observer.BeginSection(sectionKey, baseMessage, elements);
+            }
+            return sectionKey;
+        }
+
+        public void EndSection(Guid sectionKey, string newTitle = "", bool collapse = false)
+        {
+            foreach (var observer in ViewModelObservers())
+            {
+                observer.EndSection(sectionKey, newTitle, collapse);
+            }
+        }
+
+        private IEnumerable<IViewModelObserver> ViewModelObservers()
+        {
+            return this.Select(o => (o as IViewModelObserver)).Where(o => o != null);
+        }
+    }
+}

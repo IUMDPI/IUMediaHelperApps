99a68cc669422013d983b7b45e6d28fffc6178db
Brent Moberly <bmoberly@iu.edu>
2015-07-30 14:40:19 -0400

    should resolve descriptions for metadata properties

--- Packager/Models/PodMetadataModels/ConsolidatedPodMetadata.cs
+++ Packager/Models/PodMetadataModels/ConsolidatedPodMetadata.cs
@@ -1,48 +1,112 @@
-﻿using System.CodeDom;
-using System.Collections.Generic;
+﻿using System.ComponentModel;
 using System.Linq;
-using Packager.Attributes;
-using Packager.Observers;
-using Packager.Providers;
-using RestSharp.Deserializers;
+using System.Reflection;
 
 namespace Packager.Models.PodMetadataModels
 {
     public class ConsolidatedPodMetadata
     {
-        private PodMetadata Metadata { get; set; }
-        private Details Details { get; set; }
-        private Assignment Assignment { get; set; }
-        private TechnicalMetadata TechnicalMetadata { get; set; }
-
         public ConsolidatedPodMetadata(PodMetadata metadata)
         {
             Metadata = metadata;
-            Details = metadata.Data != null ? metadata.Data.Object.Details : null;
-            Assignment = metadata.Data != null ? metadata.Data.Object.Assignment : null;
-            TechnicalMetadata = metadata.Data != null ? metadata.Data.Object.TechnicalMetadata : null;
-            DigitalProvenance = metadata.Data != null ? metadata.Data.Object.DigitalProvenance : null;
         }
 
-        public DigitalProvenance DigitalProvenance { get; set; }
+        private PodMetadata Metadata { get; set; }
 
-        public string Identifier { get { return Metadata.Data.Object.Details.Id; } }
-        public bool Success { get { return Metadata.Success; } }
-        public string Message { get { return Metadata.Message; } }
-        public string CallNumber { get { return Details.CallNumber; } }
-        public string Title { get { return Details.Title; } }
-        public string Unit { get { return Assignment.Unit; } }
-        public string Barcode { get { return Details.MdpiBarcode; } }
-        public string Brand { get { return TechnicalMetadata.TapeStockBrand; } }
-        public string CarrierType { get { return TechnicalMetadata.Format; } }
-        public string DirectionsRecorded { get { return TechnicalMetadata.DirectionsRecorded; } }
+        private Details Details
+        {
+            get { return Metadata.Data.Object.Details; }
+        }
 
-        public string CleaningDate { get { return DigitalProvenance.CleaningDate; } }
-        public string CleaningComment { get { return DigitalProvenance.CleaningComment; } }
-        public string BakingDate { get { return DigitalProvenance.Baking; } }
-        public bool Repaired { get { return DigitalProvenance.Repaired; } }
+        private Assignment Assignment
+        {
+            get { return Metadata.Data.Object.Assignment; }
+        }
 
-        public string DigitizingEntity { get { return DigitalProvenance.DigitizingEntity; } }
+        private TechnicalMetadata TechnicalMetadata
+        {
+            get { return Metadata.Data.Object.TechnicalMetadata; }
+        }
+
+        public DigitalProvenance DigitalProvenance
+        {
+            get { return Metadata.Data.Object.DigitalProvenance; }
+        }
+
+        public string Identifier
+        {
+            get { return Metadata.Data.Object.Details.Id; }
+        }
+
+        public bool Success
+        {
+            get { return Metadata.Success; }
+        }
+
+        public string Message
+        {
+            get { return Metadata.Message; }
+        }
+
+        public string CallNumber
+        {
+            get { return Details.CallNumber; }
+        }
+
+        public string Title
+        {
+            get { return Details.Title; }
+        }
+
+        public string Unit
+        {
+            get { return Assignment.Unit; }
+        }
+
+        public string Barcode
+        {
+            get { return Details.MdpiBarcode; }
+        }
+
+        public string Brand
+        {
+            get { return TechnicalMetadata.TapeStockBrand; }
+        }
+
+        public string CarrierType
+        {
+            get { return TechnicalMetadata.Format; }
+        }
+
+        public string DirectionsRecorded
+        {
+            get { return TechnicalMetadata.DirectionsRecorded; }
+        }
+
+        public string CleaningDate
+        {
+            get { return DigitalProvenance.CleaningDate; }
+        }
+
+        public string CleaningComment
+        {
+            get { return DigitalProvenance.CleaningComment; }
+        }
+
+        public string BakingDate
+        {
+            get { return DigitalProvenance.Baking; }
+        }
+
+        public bool Repaired
+        {
+            get { return DigitalProvenance.Repaired; }
+        }
+
+        public string DigitizingEntity
+        {
+            get { return DigitalProvenance.DigitizingEntity; }
+        }
 
         public string PlaybackSpeeds
         {
@@ -54,10 +118,15 @@ namespace Packager.Models.PodMetadataModels
             get { return GetBoolValuesAsList(TechnicalMetadata.TrackConfiguration); }
         }
 
-        public string SoundFields { get { return GetBoolValuesAsList(TechnicalMetadata.SoundField); } }
+        public string SoundFields
+        {
+            get { return GetBoolValuesAsList(TechnicalMetadata.SoundField); }
+        }
 
-        public string TapeThicknesses { get { return GetBoolValuesAsList(TechnicalMetadata.TapeThickness); } }
-
+        public string TapeThicknesses
+        {
+            get { return GetBoolValuesAsList(TechnicalMetadata.TapeThickness); }
+        }
 
         private static string GetBoolValuesAsList(object instance)
         {
@@ -66,10 +135,16 @@ namespace Packager.Models.PodMetadataModels
                 return string.Empty;
             }
 
-            var properties = instance.GetType().GetProperties().Where(p => p.PropertyType == typeof(bool));
-            var results = (properties.Where(property => (bool)property.GetValue(instance))
-                .Select(property => property.Name)).Distinct().ToList();
+            var properties = instance.GetType().GetProperties().Where(p => p.PropertyType == typeof (bool));
+            var results = (properties.Where(property => (bool) property.GetValue(instance))
+                .Select(GetNameOrDescription)).Distinct().ToList();
             return string.Join(", ", results);
+        }
+
+        private static string GetNameOrDescription(MemberInfo propertyInfo)
+        {
+            var description = propertyInfo.GetCustomAttribute<DescriptionAttribute>();
+            return description == null ? propertyInfo.Name : description.Description;
         }
     }
 }

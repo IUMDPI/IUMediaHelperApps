966e9d8af4d2795d9922f3871fc982ee42a1fcfb
Brent Moberly <bmoberly@iu.edu>
2015-08-05 17:01:55 -0400

    adding boolean reason

---
+++ Packager/Models/ResultModels/BooleanReason.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Packager.Models.ResultModels
+{
+    public class BooleanReason
+    {
+        private BooleanReason(bool succeeded, Exception issue)
+        {
+            Succeeded = succeeded;
+            Reason = issue;
+        }
+
+        public BooleanReason(Exception issue) : this(false, issue)
+        {
+        }
+
+        public bool Succeeded { get; set; }
+        public Exception Reason { get; set; }
+
+        public static BooleanReason Success()
+        {
+            return new BooleanReason(true, null);
+        }
+    }
+}

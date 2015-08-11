01b997cbc4eed3acf179ca220cc1a560731ddb93
Brent Moberly <bmoberly@iu.edu>
2015-08-11 11:38:17 -0400

    adding new factories to refine program logic / testing

---
+++ Packager/Utilities/IIngestDataFactory.cs
@@ -0,0 +1,41 @@
+﻿using Packager.Exceptions;
+using Packager.Extensions;
+using Packager.Models.FileModels;
+using Packager.Models.OutputModels;
+using Packager.Models.PodMetadataModels;
+
+namespace Packager.Utilities
+{
+    public interface IIngestDataFactory
+    {
+        IngestData Generate(ConsolidatedPodMetadata podMetadata, AbstractFileModel masterFileModel);
+    }
+
+    public class IngestDataFactory : IIngestDataFactory
+    {
+        public IngestData Generate(ConsolidatedPodMetadata podMetadata, AbstractFileModel masterFileModel)
+        {
+            var digitalFileProvenance = podMetadata.FileProvenances.GetFileProvenance(masterFileModel);
+            if (digitalFileProvenance == null)
+            {
+                throw new OutputXmlException("No digital file provenance found for {0}", masterFileModel.ToFileName());
+            }
+
+            return new IngestData
+            {
+                XsiType = string.Format("{0}Ingest", podMetadata.Format),
+                AdManufacturer = digitalFileProvenance.AdManufacturer,
+                AdModel = digitalFileProvenance.AdModel,
+                AdSerialNumber = digitalFileProvenance.AdSerialNumber,
+                Comments = digitalFileProvenance.Comment,
+                CreatedBy = digitalFileProvenance.CreatedBy,
+                PlayerManufacturer = digitalFileProvenance.PlayerManufacturer,
+                PlayerModel = digitalFileProvenance.PlayerModel,
+                PlayerSerialNumber = digitalFileProvenance.PlayerSerialNumber,
+                ExtractionWorkstation = digitalFileProvenance.ExtractionWorkstation,
+                SpeedUsed = digitalFileProvenance.SpeedUsed,
+                Date = digitalFileProvenance.DateDigitized
+            };
+        }
+    }
+}
---
+++ Packager/Utilities/ISideDataFactory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Packager.Exceptions;
+using Packager.Extensions;
+using Packager.Models.FileModels;
+using Packager.Models.OutputModels;
+using Packager.Models.PodMetadataModels;
+
+namespace Packager.Utilities
+{
+    public interface ISideDataFactory
+    {
+        SideData[] Generate(ConsolidatedPodMetadata podMetadata, IEnumerable<ObjectFileModel> filesToProcess, string processingDirectory);
+    }
+
+    public class SideDataFactory : ISideDataFactory
+    {
+        private IHasher Hasher { get; set; }
+        private IIngestDataFactory IngestDataFactory { get; set; }
+
+        public SideData[] Generate(ConsolidatedPodMetadata podMetadata, IEnumerable<ObjectFileModel> filesToProcess, string processingDirectory)
+        {
+            var sideGroupings = filesToProcess.GroupBy(f => f.SequenceIndicator).OrderBy(g => g.Key).ToList();
+            if (!sideGroupings.Any())
+            {
+                throw new OutputXmlException("Could not determine side groupings");
+            }
+
+            return sideGroupings.Select(grouping => new SideData
+            {
+                Side = grouping.Key.ToString("D2", CultureInfo.InvariantCulture),
+                Files = grouping.Select(m => GetFileData(m, processingDirectory)).ToList(),
+                Ingest = IngestDataFactory.Generate(podMetadata, grouping.GetPreservationOrIntermediateModel()),
+                ManualCheck = "No"
+            }).ToArray();
+        }
+
+        private FileData GetFileData(AbstractFileModel objectFileModel, string processingDirectory)
+        {
+            var filePath = Path.Combine(processingDirectory, objectFileModel.ToFileName());
+
+            return new FileData
+            {
+                Checksum = Hasher.Hash(filePath),
+                FileName = Path.GetFileName(filePath)
+            };
+        }
+    }
+}

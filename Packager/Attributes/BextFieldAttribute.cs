3ecf394ac5ecb2e8a74dce1f57d7327165d02e87
Brent Moberly <bmoberly@iu.edu>
2015-09-08 13:09:19 -0400

    adding bext field attribute and decorating model

---
+++ Packager/Attributes/BextFieldAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Packager.Utilities;
+
+namespace Packager.Attributes
+{
+    public class BextFieldAttribute:Attribute
+    {
+        public BextFields Field { get; set; }
+
+        public BextFieldAttribute(BextFields field)
+        {
+            Field = field;
+        }
+    }
+}

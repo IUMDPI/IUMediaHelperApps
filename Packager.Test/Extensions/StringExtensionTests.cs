93f15455849eb5a6392a22cd824dc893f59f00d3
Brent Moberly <bmoberly@iu.edu>
2015-10-26 10:05:54 -0400

    fix issue normalizing for command-line

---
+++ Packager.Test/Extensions/StringExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Packager.Extensions;
+
+namespace Packager.Test.Extensions
+{
+    [TestFixture]
+    public class StringExtensionTests
+    {
+        [TestCase(@"testing """, @"testing \""")]
+        public void NormalizeForCommandLineShouldEscapeQuotesCorrectly(string original, string expected)
+        {
+            var result = original.NormalizeForCommandLine();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}

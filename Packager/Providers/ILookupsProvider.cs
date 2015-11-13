9c92728f065120b81dcf5e6fcace376331824fe0
Brent Moberly <bmoberly@iu.edu>
2015-11-13 11:59:21 -0500

    re-adding lookups provider

---
+++ Packager/Providers/ILookupsProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Packager.Providers
+{
+    public enum LookupTables
+    {
+        Units,
+        PlaybackSpeeds,
+        TrackConfigurations,
+        SoundFields,
+        TapeThicknesses
+    }
+
+    public interface ILookupsProvider
+    {
+        Dictionary<string, string> Units { get; }
+        Dictionary<string, string> PlaybackSpeeds { get; }
+        Dictionary<string, string> TrackConfigurations { get; }
+        Dictionary<string, string> SoundFields { get; }
+        Dictionary<string, string> TapeThicknesses { get; }
+
+        string LookupValue(LookupTables table, string value);
+        string[] LookupValue(LookupTables table, string[] values);
+    }
+}

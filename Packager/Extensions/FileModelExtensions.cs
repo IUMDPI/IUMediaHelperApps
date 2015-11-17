7d24a07fc8015d75609a20c0dfe1365c971ade5f
Brent Moberly <bmoberly@iu.edu>
2015-11-17 11:28:52 -0500

    add remove duplicates extension method

--- Packager/Extensions/FileModelExtensions.cs
+++ Packager/Extensions/FileModelExtensions.cs
@@ -20,22 +20,25 @@ namespace Packager.Extensions
         {
             return fileModel is UnknownFileModel;
         }
-
-
+        
         public static ObjectFileModel GetPreservationOrIntermediateModel<T>(this IGrouping<T, ObjectFileModel> grouping)
         {
             var preservationIntermediate = grouping.SingleOrDefault(m => m.IsPreservationIntermediateVersion());
             return preservationIntermediate ?? grouping.SingleOrDefault(m => m.IsPreservationVersion());
         }
-
-       
-
+        
         public static ObjectFileModel GetPreservationOrIntermediateModel(this IEnumerable<ObjectFileModel> models)
         {
             var list = models.ToList();
 
             var preservationIntermediate = list.FirstOrDefault(m => m.IsPreservationIntermediateVersion());
             return preservationIntermediate ?? list.FirstOrDefault(m => m.IsPreservationVersion());
+        }
+
+        public static List<ObjectFileModel> RemoveDuplicates(this List<ObjectFileModel> models)
+        {
+            return models.GroupBy(o => o.ToFileName())
+                .Select(g => g.First()).ToList();
         }
     }
 }

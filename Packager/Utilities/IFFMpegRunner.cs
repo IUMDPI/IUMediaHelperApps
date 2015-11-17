fdfb9f83bc53c530ea91cd17c7639d86444dd10b
Brent Moberly <bmoberly@iu.edu>
2015-11-17 17:37:12 -0500

    cleanup code

--- Packager/Utilities/IFFMpegRunner.cs
+++ Packager/Utilities/IFFMpegRunner.cs
@@ -2,14 +2,16 @@
 using System.Threading.Tasks;
 using Packager.Models.EmbeddedMetadataModels;
 using Packager.Models.FileModels;
-using Packager.Models.PodMetadataModels;
 
 namespace Packager.Utilities
 {
     public interface IFFMPEGRunner
     {
         string FFMPEGPath { get; set; }
-        
+
+        string ProdOrMezzArguments { get; }
+        string AccessArguments { get; }
+
         Task<string> GetFFMPEGVersion();
 
         Task Normalize(ObjectFileModel original, AbstractEmbeddedMetadata metadata);
@@ -19,9 +21,5 @@ namespace Packager.Utilities
         Task<ObjectFileModel> CreateProdOrMezzDerivative(ObjectFileModel original, ObjectFileModel target, AbstractEmbeddedMetadata metadata);
 
         Task<ObjectFileModel> CreateAccessDerivative(ObjectFileModel original);
-
-        string ProdOrMezzArguments { get; }
-        string AccessArguments { get; }
-
     }
 }

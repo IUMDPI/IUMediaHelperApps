d5ebde55a1aeb78c46c7379c8902fc3fc8b66101
Brent Moberly <bmoberly@iu.edu>
2015-11-19 14:12:59 -0500

    clean up code

--- Packager.Test/Extensions/ArrayExtensionTests.cs
+++ Packager.Test/Extensions/ArrayExtensionTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
 using Packager.Extensions;
 
@@ -11,34 +7,25 @@ namespace Packager.Test.Extensions
     [TestFixture]
     public class ArrayExtensionTests
     {
-
-        [Test]
-        public void FromIndexShouldReturnCorrectValues()
+        [TestCase(1, "1 member")]
+        [TestCase(2, "2 members")]
+        [TestCase(0, "0 members")]
+        public void IntegerToSingularOrPluralShouldReturnCorrectValues(int value, string expected)
         {
-            var parts = new []{"part1", "part2"};
-            Assert.That(parts.FromIndex(0, "default value"), Is.EqualTo(parts[0]));
-            Assert.That(parts.FromIndex(1, "default value"), Is.EqualTo(parts[1]));
-            Assert.That(parts.FromIndex(2, "default value"), Is.EqualTo("default value"));
+            Assert.That(value.ToSingularOrPlural("member", "members"), Is.EqualTo(expected));
         }
 
         [Test]
-        public void EnumerableToSingularOrPluralShouldReturnSingularIfOneMemberPresent()
+        public void DictionaryToSingularOrPluralShouldReturnPluralIfMoreThanOneMemberPresent()
         {
-            var members = new[] {"one"};
-            Assert.That(members.ToSingularOrPlural("member", "member"), Is.EqualTo("1 member"));
-        }
-
-        [Test]
-        public void EnumerableToSingularOrPluralShouldReturnPluralIfMoreThanOneMemberPresent()
-        {
-            var members = new[] { "one", "two" };
+            var members = new Dictionary<string, string> {{"one", "one"}, {"two", "two"}};
             Assert.That(members.ToSingularOrPlural("member", "members"), Is.EqualTo("2 members"));
         }
 
         [Test]
-        public void EnumerableToSingularOrPluralShouldReturnPluralIfNoMembersPresent()
+        public void DictionaryToSingularOrPluralShouldReturnPluralIfNoMembersPresent()
         {
-            var members = new string[0];
+            var members = new Dictionary<string, string>();
             Assert.That(members.ToSingularOrPlural("member", "members"), Is.EqualTo("0 members"));
         }
 
@@ -50,25 +37,33 @@ namespace Packager.Test.Extensions
         }
 
         [Test]
-        public void DictionaryToSingularOrPluralShouldReturnPluralIfMoreThanOneMemberPresent()
+        public void EnumerableToSingularOrPluralShouldReturnPluralIfMoreThanOneMemberPresent()
         {
-            var members = new Dictionary<string, string> { { "one", "one" }, {"two", "two"} };
+            var members = new[] {"one", "two"};
             Assert.That(members.ToSingularOrPlural("member", "members"), Is.EqualTo("2 members"));
         }
 
         [Test]
-        public void DictionaryToSingularOrPluralShouldReturnPluralIfNoMembersPresent()
+        public void EnumerableToSingularOrPluralShouldReturnPluralIfNoMembersPresent()
         {
-            var members = new Dictionary<string, string>();
+            var members = new string[0];
             Assert.That(members.ToSingularOrPlural("member", "members"), Is.EqualTo("0 members"));
         }
 
-        [TestCase(1, "1 member")]
-        [TestCase(2, "2 members")]
-        [TestCase(0, "0 members")]
-        public void IntegerToSingularOrPluralShouldReturnCorrectValues(int value, string expected)
+        [Test]
+        public void EnumerableToSingularOrPluralShouldReturnSingularIfOneMemberPresent()
         {
-            Assert.That(value.ToSingularOrPlural("member", "members"), Is.EqualTo(expected));
+            var members = new[] {"one"};
+            Assert.That(members.ToSingularOrPlural("member", "member"), Is.EqualTo("1 member"));
+        }
+
+        [Test]
+        public void FromIndexShouldReturnCorrectValues()
+        {
+            var parts = new[] {"part1", "part2"};
+            Assert.That(parts.FromIndex(0, "default value"), Is.EqualTo(parts[0]));
+            Assert.That(parts.FromIndex(1, "default value"), Is.EqualTo(parts[1]));
+            Assert.That(parts.FromIndex(2, "default value"), Is.EqualTo("default value"));
         }
 
 

ed432c2185770be5331f255d389e5c9ff6817e24
Brent Moberly <bmoberly@iu.edu>
2016-01-04 11:38:09 -0500

    wiring in new fields (for import)

--- Packager/Models/PodMetadataModels/DigitalAudioFile.cs
+++ Packager/Models/PodMetadataModels/DigitalAudioFile.cs
@@ -9,11 +9,21 @@ namespace Packager.Models.PodMetadataModels
     {
         [Required]
         public string SpeedUsed { get; set; }
-        
+        public string TapeFluxivity { get; set; }
+        public string AnalogOutputVoltage { get; set; }
+        public string Peak { get; set; }
+        public string StylusSize { get; set; }
+        public string TurnOver { get; set; }
+
         public override void ImportFromXml(XElement element, ILookupsProvider lookupsProvider)
         {
             base.ImportFromXml(element, lookupsProvider);
             SpeedUsed = element.ToStringValue("speed_used");
+            TapeFluxivity = element.ToStringValue("tape_fluxivity");
+            AnalogOutputVoltage = element.ToStringValue("analog_output_voltage");
+            Peak = element.ToStringValue("peak");
+            StylusSize = element.ToStringValue("stylus_size");
+            TurnOver = element.ToStringValue("turnover");
         }
     }
 }

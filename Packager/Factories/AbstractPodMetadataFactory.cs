02422c5fbc5f8e8c4e4d1cc7caa894a4e9d222d6
Brent Moberly <bmoberly@iu.edu>
2016-01-13 16:45:11 -0500

    remove unused class

--- Packager/Factories/AbstractPodMetadataFactory.cs
+++
@@ -1,8 +0,0 @@
-namespace Packager.Factories
-{
-    public abstract class AbstractPodMetadataFactory<T> : IPodMetadataFactory<T> 
-    {
-        public abstract T Generate(string xml);
-
-    }
-}

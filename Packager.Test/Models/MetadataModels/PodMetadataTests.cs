42832c4512c9906225e4f00d25edb33619c8e387
Brent Moberly <bmoberly@iu.edu>
2016-01-15 16:24:25 -0500

    adding unit tests

--- Packager.Test/Models/MetadataModels/PodMetadataTests.cs
+++ Packager.Test/Models/MetadataModels/PodMetadataTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Linq;
 using NSubstitute;
 using NUnit.Framework;
 using Packager.Factories;
@@ -14,24 +16,53 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
         {
             Factory = Substitute.For<IImportableFactory>();
             Element = new XElement("pod");
+
+            Factory.ResolveDamage(Element, "data/object/technical_metadata/damage").Returns("damage value");
+            Factory.ResolvePreservationProblems(Element, "data/object/technical_metadata/preservation_problems")
+                .Returns("preservation problems value");
+            Factory.ToUtcDateTimeValue(Element, "data/object/digital_provenance/baking_date")
+                .Returns(new DateTime(2016, 1, 1));
+            Factory.ToStringValue(Element, "data/object/details/mdpi_barcode").Returns("barcode value");
+            Factory.ToStringValue(Element, "data/object/details/call_number").Returns("call number value");
+
+            Factory.ToStringValue(Element, "data/object/digital_provenance/cleaning_comment").Returns("cleaning comment value");
+            Factory.ToUtcDateTimeValue(Element, "data/object/digital_provenance/cleaning_date")
+                .Returns(new DateTime(2016, 2, 1));
+            Factory.ToStringValue(Element, "data/object/digital_provenance/digitizing_entity").Returns("digitizing entity value");
+            Factory.ToStringValue(Element, "data/object/details/id").Returns("identifier value");
+            Factory.ToStringValue(Element, "data/object/digital_provenance/repaired").Returns("No"); //todo: fix this so it's a factory method and is more easily mockable
+
+            Factory.ToStringValue(Element, "data/object/details/title").Returns("title value");
+            Factory.ToStringValue(Element, "data/object/assignment/unit").Returns("unit value");
         }
 
+        private string FormatValue { get; set; }
         private IImportableFactory Factory { get; set; }
         private XElement Element { get; set; }
-
+        private AbstractPodMetadata Instance { get; set; }
 
         public class WhenImportingOpenReelAudioPodMetadata : AbstractPodMetadataTests
         {
+         
+            private AudioPodMetadata AudioPodMetadata => Instance as AudioPodMetadata;
             [SetUp]
             public override void BeforeEach()
             {
                 base.BeforeEach();
 
                 // need to make factory produce correct value for format
-                Factory.ToStringValue(Element, "data/object/details/format").Returns("open reel audio tape");
+                FormatValue = "open reel audio tape";
+                Factory.ToStringValue(Element, "data/object/details/format").Returns(FormatValue);
+                
+                Factory.ResolvePlaybackSpeed(Element, "data/object/technical_metadata/playback_speed").Returns("speed value");
+                Factory.ResolveSoundField(Element, "data/object/technical_metadata/sound_field").Returns("sound field value");
+                Factory.ResolveTapeThickness(Element, "data/object/technical_metadata/tape_thickness").Returns("tape thickness value");
+                Factory.ResolveTrackConfiguration(Element, "data/object/technical_metadata/track_configuration").Returns("track configuration value");
+                Factory.ToStringValue(Element, "data/object/technical_metadata/tape_stock_brand").Returns("brand value");
+                Factory.ToStringValue(Element, "data/object/technical_metadata/directions_recorded").Returns("directions recorded value");
 
-                var instance = new AudioPodMetadata();
-                instance.ImportFromXml(Element, Factory);
+                Instance = new AudioPodMetadata();
+                Instance.ImportFromXml(Element, Factory);
             }
             
             [Test]
@@ -41,15 +72,33 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
             }
 
             [Test]
+            public void ItShouldSetPlaybackSpeedCorrectly()
+            {
+                Assert.That(AudioPodMetadata.PlaybackSpeed, Is.EqualTo("speed value"));
+            }
+
+            [Test]
             public void ItShouldCallFactoryToResolveSoundField()
             {
                 Factory.Received().ResolveSoundField(Element, "data/object/technical_metadata/sound_field");
             }
 
             [Test]
+            public void ItShouldSetSoundFieldCorrectly()
+            {
+                Assert.That(AudioPodMetadata.SoundField, Is.EqualTo("sound field value"));
+            }
+
+            [Test]
             public void ItShouldCallFactoryToResolveTapeThickness()
             {
                 Factory.Received().ResolveTapeThickness(Element, "data/object/technical_metadata/tape_thickness");
+            }
+
+            [Test]
+            public void ItShouldSetTapeThicknessCorrectly()
+            {
+                Assert.That(AudioPodMetadata.TapeThickness, Is.EqualTo("tape thickness value"));
             }
 
             [Test]
@@ -60,9 +109,21 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
             }
 
             [Test]
+            public void ItShouldSetTrackConfigurationCorrectly()
+            {
+                Assert.That(AudioPodMetadata.TrackConfiguration, Is.EqualTo("track configuration value"));
+            }
+
+            [Test]
             public void ItShouldUseCorrectPathToResolveBrand()
             {
                 Factory.Received().ToStringValue(Element, "data/object/technical_metadata/tape_stock_brand");
+            }
+
+            [Test]
+            public void ItShouldSetBrandCorrectly()
+            {
+                Assert.That(AudioPodMetadata.Brand, Is.EqualTo("brand value"));
             }
 
             [Test]
@@ -70,20 +131,32 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
             {
                 Factory.Received().ToStringValue(Element, "data/object/technical_metadata/directions_recorded");
             }
+
+            [Test]
+            public void ItShouldSetDirectionsRecordedCorrectly()
+            {
+                Assert.That(AudioPodMetadata.DirectionsRecorded, Is.EqualTo("directions recorded value"));
+            }
         }
 
         public class WhenImportingLacquerDiscAudioPodMetadata : AbstractPodMetadataTests
         {
+            private AudioPodMetadata AudioPodMetadata => Instance as AudioPodMetadata;
+
             [SetUp]
             public override void BeforeEach()
             {
                 base.BeforeEach();
 
-                // need to make factory produce correct value for format
-                Factory.ToStringValue(Element, "data/object/details/format").Returns("lacquer disc");
+                FormatValue = "lacquer disc";
+                Factory.ToStringValue(Element, "data/object/details/format").Returns(FormatValue);
 
-                var instance = new AudioPodMetadata();
-                instance.ImportFromXml(Element, Factory);
+                // need to make factory produce correct value for format
+                Factory.ToStringValue(Element, "data/object/technical_metadata/speed", " rpm").Returns("speed value");
+                Factory.ToStringValue(Element, "data/object/technical_metadata/sound_field").Returns("sound field value");
+
+                Instance = new AudioPodMetadata();
+                Instance.ImportFromXml(Element, Factory);
             }
 
             [Test]
@@ -93,22 +166,42 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
             }
 
             [Test]
+            public void PlaybackSpeedShouldHaveCorrectValue()
+            {
+                Assert.That(AudioPodMetadata.PlaybackSpeed, Is.EqualTo("speed value"));
+            }
+
+            [Test]
             public void ItShouldCallFactoryToResolveSoundField()
             {
                 Factory.Received().ToStringValue(Element, "data/object/technical_metadata/sound_field");
+            }
+
+            [Test]
+            public void SoundfieldShouldHaveCorrectValue()
+            {
+                Assert.That(AudioPodMetadata.SoundField, Is.EqualTo("sound field value"));
             }
         }
 
 
         public class WhenImportingVideoPodMetadata : AbstractPodMetadataTests
         {
+            public VideoPodMetadata VideoPodMetadata => Instance as VideoPodMetadata;
+
             [SetUp]
             public override void BeforeEach()
             {
                 base.BeforeEach();
 
-                var instance = new VideoPodMetadata();
-                instance.ImportFromXml(Element, Factory);
+                FormatValue = "8mm video";
+                Factory.ToStringValue(Element, "data/object/details/format").Returns(FormatValue);
+                Factory.ToStringValue(Element, "data/object/technical_metadata/image_format").Returns("image format value");
+                Factory.ToStringValue(Element, "data/object/technical_metadata/format_version").Returns("definition value");
+                Factory.ToStringValue(Element, "data/object/technical_metadata/recording_standard").Returns("recording standard value");
+
+                Instance = new VideoPodMetadata();
+                Instance.ImportFromXml(Element, Factory);
             }
 
             [Test]
@@ -117,17 +210,34 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
                 Factory.Received().ToStringValue(Element, "data/object/technical_metadata/image_format");
             }
 
+            [Test]
+            public void ItShouldSetImageFormatCorrectly()
+            {
+                Assert.That(VideoPodMetadata.ImageFormat, Is.EqualTo("image format value"));
+            }
 
             [Test]
-            public void ItShouldCallFactoryCorrectlyToResolveRecordingFormatVersion()
+            public void ItShouldCallFactoryCorrectlyToResolveFormatVersion()
             {
                 Factory.Received().ToStringValue(Element, "data/object/technical_metadata/format_version");
+            }
+
+            [Test]
+            public void ItShouldSetDefinitionCorrectly()
+            {
+                Assert.That(VideoPodMetadata.Definition, Is.EqualTo("definition value"));
             }
 
             [Test]
             public void ItShouldCallFactoryCorrectlyToResolveRecordingStandard()
             {
                 Factory.Received().ToStringValue(Element, "data/object/technical_metadata/recording_standard");
+            }
+
+            [Test]
+            public void ItShouldSetFormatRecordingStandardCorrectly()
+            {
+                Assert.That(VideoPodMetadata.RecordingStandard, Is.EqualTo("recording standard value"));
             }
         }
 
@@ -139,9 +249,21 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
         }
 
         [Test]
+        public void ItShouldSetPreservationProblemsCorrectly()
+        {
+            Assert.That(Instance.PreservationProblems, Is.EqualTo("preservation problems value"));
+        }
+
+        [Test]
         public void ItShouldCallFactoryCorrectlyToResolveDamage()
         {
             Factory.Received().ResolveDamage(Element, "data/object/technical_metadata/damage");
+        }
+
+        [Test]
+        public void ItShouldSetDamageCorrectly()
+        {
+            Assert.That(Instance.Damage, Is.EqualTo("damage value"));
         }
 
         [Test]
@@ -151,9 +273,21 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
         }
 
         [Test]
+        public void ItShouldSetBakingDateCorrectly()
+        {
+            Assert.That(Instance.BakingDate, Is.EqualTo(new DateTime(2016,1,1)));
+        }
+
+        [Test]
         public void ItShouldUseCorrectPathToResolveBarcode()
         {
             Factory.Received().ToStringValue(Element, "data/object/details/mdpi_barcode");
+        }
+
+        [Test]
+        public void ItShouldSetBarcodeCorrectly()
+        {
+            Assert.That(Instance.Barcode, Is.EqualTo("barcode value"));
         }
 
         [Test]
@@ -163,9 +297,21 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
         }
 
         [Test]
+        public void ItShouldSetCallNumberCorrectly()
+        {
+            Assert.That(Instance.CallNumber, Is.EqualTo("call number value"));
+        }
+
+        [Test]
         public void ItShouldUseCorrectPathToResolveCleaningComment()
         {
             Factory.Received().ToStringValue(Element, "data/object/digital_provenance/cleaning_comment");
+        }
+
+        [Test]
+        public void ItShouldSetCleaningCommentCorrectly()
+        {
+            Assert.That(Instance.CleaningComment, Is.EqualTo("cleaning comment value"));
         }
 
         [Test]
@@ -175,9 +321,21 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
         }
 
         [Test]
+        public void ItShouldSetCleaningDateCorrectly()
+        {
+            Assert.That(Instance.CleaningDate, Is.EqualTo(new DateTime(2016,2,1)));
+        }
+
+        [Test]
         public void ItShouldUseCorrectPathToResolveDigitizingEntity()
         {
             Factory.Received().ToStringValue(Element, "data/object/digital_provenance/digitizing_entity");
+        }
+
+        [Test]
+        public void ItShouldSetDigitizingEntityCorrectly()
+        {
+            Assert.That(Instance.DigitizingEntity, Is.EqualTo("digitizing entity value"));
         }
 
         [Test]
@@ -187,9 +345,21 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
         }
 
         [Test]
+        public void ItShouldSetFormatCorrectly()
+        {
+            Assert.That(Instance.Format, Is.EqualTo(FormatValue));
+        }
+
+        [Test]
         public void ItShouldUseCorrectPathToResolveIdentifier()
         {
             Factory.Received().ToStringValue(Element, "data/object/details/id");
+        }
+
+        [Test]
+        public void ItShouldSetIdentifierCorrectly()
+        {
+            Assert.That(Instance.Identifier, Is.EqualTo("identifier value"));
         }
 
         [Test]
@@ -199,15 +369,33 @@ namespace Packager.Test.Models.MetadataModels.PodMetadataTests
         }
 
         [Test]
+        public void ItShouldSetRepairedCorrectly()
+        {
+            Assert.That(Instance.Repaired, Is.EqualTo("No"));
+        }
+
+        [Test]
         public void ItShouldUseCorrectPathToResolveTitle()
         {
             Factory.Received().ToStringValue(Element, "data/object/details/title");
         }
 
         [Test]
+        public void ItShouldSetTitleCorrectly()
+        {
+            Assert.That(Instance.Title, Is.EqualTo("title value"));
+        }
+
+        [Test]
         public void ItShouldUseCorrectPathToResolveUnit()
         {
             Factory.Received().ToStringValue(Element, "data/object/assignment/unit");
+        }
+
+        [Test]
+        public void ItShouldSetUnitCorrectly()
+        {
+            Assert.That(Instance.Unit, Is.EqualTo("unit value"));
         }
     }
 }

95d1f08e8c079c146a918439aafa79b331b0e13f
Brent Moberly <bmoberly@iu.edu>
2016-02-08 10:19:41 -0500

    fix name of qc tools file

--- Packager/Models/FileModels/QualityControlFile.cs
+++ Packager/Models/FileModels/QualityControlFile.cs
@@ -2,10 +2,13 @@
 {
     public class QualityControlFile : AbstractFile
     {
+        private readonly string _originalExtension;
+
         public QualityControlFile(AbstractFile original) : base(original)
         {
             FileUse = original.FileUse;
             FullFileUse = original.FullFileUse;
+            _originalExtension = original.Extension;
         }
 
         public override string FileUse { get; }
@@ -13,9 +16,14 @@
 
         public override string Extension => ".qctools.xml.gz";
 
+        public override string ToFileName()
+        {
+            return $"{ToFileNameWithoutExtension()}{_originalExtension}{Extension}";
+        }
+
         public string ToIntermediateFileName()
         {
-            return $"{ToFileNameWithoutExtension()}.qctools.xml";
+            return $"{ToFileNameWithoutExtension()}{_originalExtension}.qctools.xml";
         }
     }
 }

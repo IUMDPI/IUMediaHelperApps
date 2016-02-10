8d062d66767d5220eb9be2b2fcfc00125e437457
Brent Moberly <bmoberly@iu.edu>
2016-02-10 16:39:19 -0500

    remove unused methods from IFileProvider

--- Packager/Providers/IFileProvider.cs
+++ Packager/Providers/IFileProvider.cs
@@ -6,24 +6,14 @@ namespace Packager.Providers
 {
     public interface IFileProvider
     {
-        void Move(string sourceFileName, string destFileName);
-        void Copy(string sourceFileName, string destFileName);
-        void WriteAllText(string path, string contents);
-
         Task CopyFileAsync(string sourceFileName, string destFileName);
         Task MoveFileAsync(string sourceFileName, string destFileName);
         bool FileExists(string path);
         bool FileDoesNotExist(string path);
         FileInfo GetFileInfo(string path);
-        FileVersionInfo GetFileVersionInfo(string path);
-        string GetFileVersion(string path);
-
         void AppendText(string path, string text);
-
         string ReadAllText(string path);
-
         Task ArchiveFile(string filePath, string archivePath);
-
         T Deserialize<T>(string filePath);
     }
 }

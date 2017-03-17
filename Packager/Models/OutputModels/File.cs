36f4f870cba43231ea8ea2a36d8bbfabdc24066a
Brent Moberly <bmoberly@iu.edu>
2017-03-17 14:22:07 -0400

    File xml class serialize placeholder node or checksum

--- Packager/Models/OutputModels/File.cs
+++ Packager/Models/OutputModels/File.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using System.Xml.Serialization;
+using Common.Extensions;
 
 namespace Packager.Models.OutputModels
 {
     [Serializable]
     public class File
     {
+        public File()
+        {
+            PlaceHolder = string.Empty;     // ensures that placeholder node gets 
+                                            // serialized as <PlaceHolder />
+        }
+
+        [XmlElement("FileName", Order = 1)]
         public string FileName { get; set; }
 
-        [XmlElement("CheckSum")]
+        [XmlElement("CheckSum", Order =2)]
         public string Checksum { get; set; }
+
+        [XmlElement("PlaceHolder", Order = 3)]
+        public string PlaceHolder {get;set; }
+
+        // include Checksum node if it is set; otherwise, suppress it
+        public bool ShouldSerializeChecksum() => Checksum.IsSet();
+
+        // if checksum node is not set, include PlaceHolder node; 
+        // otherwise suppress PlaceHolder node
+        public bool ShouldSerializePlaceHolder() => Checksum.IsNotSet();    
     }
 }

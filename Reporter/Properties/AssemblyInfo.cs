425079a9c8e4ddcd74ef1441b66f7aac136bafb3
Brent Moberly <bmoberly@iu.edu>
2017-03-20 11:03:18 -0400

    update release notes, assembly info

--- Reporter/Properties/AssemblyInfo.cs
+++ Reporter/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: Guid("e0d53975-8289-40a7-86f3-54b9532b9bcf")]
 

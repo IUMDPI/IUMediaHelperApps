dcc096829ae22d5c46982721fa483ac19f6c5b8a
Brent Moberly <bmoberly@iu.edu>
2017-03-22 17:34:31 -0400

    working on batch files for Rob

--- Recorder/extract audio channels.bat
+++
@@ -1,5 +0,0 @@
-c:\dependencies\ffmpeg\ffmpeg -i %1 -map 0:0 0.wav
-c:\dependencies\ffmpeg\ffmpeg -i %1 -map 0:1 1.wav
-c:\dependencies\ffmpeg\ffmpeg -i %1 -map 0:2 2.wav
-c:\dependencies\ffmpeg\ffmpeg -i %1 -map 0:3 3.wav
-pause

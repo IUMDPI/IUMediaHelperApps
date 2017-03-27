69f7d91e69da082ba1ef97e02cfb14f8427642cc
Brent Moberly <bmoberly@iu.edu>
2017-03-27 11:51:24 -0400

    adding Common.Models.FileUsages
    enum class to contain members for known file usages + tests. Adding to
    common so can use in Recoder project.

---
+++ Common.Tests/Models/FileUsagesTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Models;
+using NUnit.Framework;
+
+namespace Common.Tests.Models
+{
+    [TestFixture]
+    public class FileUsagesTests
+    {
+        private static object[] _usagesCorrectCases =
+        {
+            new object[] {FileUsages.PreservationMaster, "pres", "Preservation Master"},
+            new object[] {FileUsages.PreservationIntermediateMaster, "presInt", "Preservation Master - Intermediate"},
+            new object[] {FileUsages.ProductionMaster, "prod", "Production Master"},
+            new object[] {FileUsages.MezzanineFile, "mezz", "Mezzanine File"},
+            new object[] {FileUsages.AccessFile, "access", "Access File"},
+            new object[] {FileUsages.LabelImageFile, "label", "Label Image File"}
+        };
+
+        private static object[] _getUsageCases =
+        {
+            new object[] {"pres",FileUsages.PreservationMaster},
+            new object[] {"presInt",FileUsages.PreservationIntermediateMaster},
+            new object[] {"prod", FileUsages.ProductionMaster},
+            new object[] {"mezz", FileUsages.MezzanineFile},
+            new object[] {"access", FileUsages.AccessFile},
+            new object[] { "label", FileUsages.LabelImageFile }
+        };
+
+        [Test, TestCaseSource(nameof(_usagesCorrectCases))]
+        public void UsagesShouldBeCorrect(FileUsages usage, string expectedFileUse, string expectedFullFileUse)
+        {
+            Assert.That(usage.FileUse, Is.EqualTo(expectedFileUse), 
+                $"File use should be {expectedFileUse}");
+            Assert.That(usage.FullFileUse, Is.EqualTo(expectedFullFileUse),
+                $"Full file use should be {expectedFullFileUse}");
+        }
+
+        [Test, TestCaseSource(nameof(_getUsageCases))]
+        public void GetUsageShouldReturnCorrectResult(string fileUse, FileUsages expected)
+        {
+            var result = FileUsages.GetUsage(fileUse);
+            Assert.That(result, Is.EqualTo(expected), $"{fileUse} should produce {expected.FullFileUse} file usage object");
+        } 
+    }
+}
---
+++ Common/Models/FileUsages.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Models
+{
+    public class FileUsages
+    {
+        protected FileUsages(string fileUse, string fullFileUse)
+        {
+            FileUse = fileUse;
+            FullFileUse = fullFileUse;
+        }
+
+        public string FileUse { get; }
+        public string FullFileUse { get; }
+
+        public static readonly FileUsages PreservationMaster = new FileUsages("pres", "Preservation Master");
+        public static readonly FileUsages PreservationIntermediateMaster = new FileUsages("presInt", "Preservation Master - Intermediate");
+        public static readonly FileUsages ProductionMaster = new FileUsages("prod", "Production Master");
+        public static readonly FileUsages MezzanineFile = new FileUsages("mezz", "Mezzanine File");
+        public static readonly FileUsages AccessFile = new FileUsages("access", "Access File");
+        public static readonly FileUsages LabelImageFile = new FileUsages("label", "Label Image File");
+
+        private static Dictionary<string , FileUsages> AllUsages = new Dictionary<string, FileUsages>
+        {
+            { PreservationMaster.FileUse.ToLowerInvariant(), PreservationMaster},
+            { PreservationIntermediateMaster.FileUse.ToLowerInvariant(), PreservationIntermediateMaster },
+            {ProductionMaster.FileUse.ToLowerInvariant(), ProductionMaster },
+            {MezzanineFile.FileUse.ToLowerInvariant(), MezzanineFile },
+            {AccessFile.FileUse.ToLowerInvariant(), AccessFile },
+            {LabelImageFile.FileUse.ToLowerInvariant(), LabelImageFile }
+        };
+
+        public static FileUsages GetUsage(string fileUse)
+        {
+            return AllUsages.ContainsKey(fileUse.ToLowerInvariant()) 
+                ? AllUsages[fileUse.ToLowerInvariant()] 
+                : null;
+        }
+    }
+}

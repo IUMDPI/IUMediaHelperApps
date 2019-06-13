6b02f9b8860db7e93929188169826f77c18f663d
Brent Moberly <bmoberly@iu.edu>
2019-06-13 11:20:11 -0400

    remove extra space

--- Packager/Factories/CodingHistory/LacquerDiscIreneCodingHistoryGenerator.cs
+++ Packager/Factories/CodingHistory/LacquerDiscIreneCodingHistoryGenerator.cs
@@ -5,7 +5,7 @@ namespace Packager.Factories.CodingHistory
 {
     public class LacquerDiscIreneCodingHistoryGenerator : AbstractCodingHistoryGenerator
     {
-        private const string Line1 = "A=ANALOG,M=Mono,T=IRENE; Lacquer Disc,";
+        private const string Line1 = "A=ANALOG,M=Mono,T=IRENE;Lacquer Disc,";
         private const string Line2 = "A=TIFF,M=Mono,T=IRENE imaging sensor,";
         private const string Line3 = "A=PCM,F=130000,W=32fp,M=Mono,T=Weaver software,";
         private const string Line4 = "A=PCM,F=96000,W=24,M=Mono,T=Izotope RX6";

7da177aa1669cc13009fdff222ee42d9820b44a4
Brent Moberly <bmoberly@iu.edu>
2023-09-14 13:41:54 -0400

    implement dat coding history generator

--- Packager/Factories/CodingHistory/DatCodingHistoryGenerator.cs
+++ Packager/Factories/CodingHistory/DatCodingHistoryGenerator.cs
@@ -1,4 +1,5 @@
-﻿using Packager.Models.FileModels;
+﻿using Packager.Extensions;
+using Packager.Models.FileModels;
 using Packager.Models.PodMetadataModels;
 
 namespace Packager.Factories.CodingHistory
@@ -6,23 +7,26 @@ namespace Packager.Factories.CodingHistory
     // TODO:
     // Here, the pres-int model should have their sound field set to "Mono".
     // Otherwise, the sound field should be set to "Stereo"
-    public class DatCodingHistoryGenerator: AbstractCodingHistoryGenerator {
+    public class DatCodingHistoryGenerator : AbstractCodingHistoryGenerator
+    {
         protected override string GenerateLine1(AudioPodMetadata metadata, DigitalAudioFile provenance, AbstractFile model)
         {
-            AssertSoundFieldSpecifiedInMetadata(metadata.SoundField);
-            return string.Format(CodingHistoryLine1Format, AnalogueFormat, metadata.SoundField,
+            //AssertSoundFieldSpecifiedInMetadata(metadata.SoundField);
+            var soundField = model.IsPreservationIntermediateVersion() ? MonoSoundField : StereoSoundField;
+            return string.Format(CodingHistoryLine1Format, DigitalFormat, soundField,
                 GeneratePlayerTextField(metadata, provenance));
         }
 
         protected override string GenerateLine2(AudioPodMetadata metadata, DigitalAudioFile provenance, AbstractFile model)
         {
-            AssertSoundFieldSpecifiedInMetadata(metadata.SoundField);
-            return string.Format(CodingHistoryLine2Format, metadata.SoundField, GenerateAdTextField(provenance));
+            var soundField = model.IsPreservationIntermediateVersion() ? MonoSoundField : StereoSoundField;
+            return string.Format(CodingHistoryLine2Format, soundField, GenerateAdTextField(provenance));
         }
 
         protected override string GenerateLine3(AudioPodMetadata metadata, DigitalAudioFile provenance, AbstractFile model)
         {
-            return string.Format(CodingHistoryLine3Format, metadata.SoundField);
+            var soundField = model.IsPreservationIntermediateVersion() ? MonoSoundField : StereoSoundField;
+            return string.Format(CodingHistoryLine3Format, soundField);
         }
 
         protected override string GenerateLine4(AudioPodMetadata metadata, DigitalAudioFile provenance, AbstractFile model)
